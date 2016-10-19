2ec58c8ed7bda41289ac630b1c1bdff37a91023e
aaroneva43 <aaroneva43@163.com>
2016-10-19 17:16:36 +0800

    todos

--- test1.js
+++ test1.js
@@ -1,15 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var Example = Vue.extend({
-        template: '<div>a: {{ a }}, b: {{ b }}</div>',
+    var Tabs = Vue.extend({
+        template: '<div class="tabar" :style="style.tabs">' +
+                      '<template v-for="item in items" track-by="_uid">'+
+                          '<a class="tab {{items.length-1 == $index ? \'tab-last\' : \'\'}}">{{ $index }} - {{ item.message }}</a>'+ 
+                      '</template>'+
+                  '<div>'
+    });
+    
+//     Vue.component('example', Tabs);
+
+    eee = new Tabs({
+        el: '#eee',
+        replace: true,
         data: function() {
             return {
-                a : 1,
-                b : 2
+                items: [{
+                    _uid: '001',
+                    message: '大苹果'
+                }, {
+                    _uid: '002',
+                    message: '大橘子'
+                }],
+                style : {
+                    tabs : {
+                        width: '500px'
+                    }
+                },
+                id: 'ddd'
             }
+
+        },
+        method : {
+            id : (function () {
+                var count = 0;
+
+                return function () {
+                    return 'x-' + count++;
+                };
+
+            })()
         }
     });
-    Vue.component('example', Example);
-
-    eee = new Example({el:'#eee'})
-
+    /*=================================================*/
 }, false);
